f6bc107d282f1e73c787d104cdbcf50fdd2e0759
Hidanath <steevpixel@gmail.com>
2025-07-18 16:49:00 +0300

    Remove unused route

--- app/products/rb.py
+++ app/products/rb.py
@@ -1,11 +1,3 @@
-class RBProductsByCategory:
-    def __init__(self, category_id: int, sorting: str | None = None):
-        self.category_id = category_id
-        self.sorting = sorting
-
-    def to_dict(self) -> dict:
-        return {'category_id': self.category_id, 'sorting': self.sorting}
-    
 class RBProducts:
     def __init__(self, id: str | None = None, name: str | None = None, category_id: int | None = None, sorting: str | None = None):
         self.id = id
--- app/products/router.py
+++ app/products/router.py
@@ -1,18 +1,14 @@
 from fastapi import APIRouter, Depends 
 from app.products.dao import ProductDAO
-from app.products.rb import RBProductsByCategory, RBProducts
+from app.products.rb import RBProducts
 from app.schema import SProduct, SProductAll
 
 router = APIRouter(prefix='/products', tags=['Товары'])
 
-@router.get("/", summary="Получить товары", response_model=list[SProduct])
+@router.get("/", summary="Получить товары по фильтру", response_model=list[SProduct])
 async def get_products_by_category(request_body: RBProducts = Depends(RBProducts)):
     return await ProductDAO.search(**request_body.to_dict())
 
-@router.get("/{category_id}", summary="Получить товары по category id", response_model=list[SProduct])
-async def get_products_by_id_or_name(request_body: RBProductsByCategory = Depends(RBProductsByCategory)):
-    return await ProductDAO.search(**request_body.to_dict())
-
-@router.get("/product/{id}", summary="Получить всю информацию о товаре по id", response_model=SProductAll)
+@router.get("/{id}", summary="Получить всю информацию о товаре по id", response_model=SProductAll)
 async def get_product_by_id(id: str):
     return await ProductDAO.search_full(id)
