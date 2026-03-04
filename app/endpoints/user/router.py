07d6fec773fb1df74e77603c5db67b5834414c27
Hidanath <steevpixel@gmail.com>
2026-03-04 21:13:31 +0200

    refactor(endpoint): change user update endpoint anwer model

--- app/endpoints/user/router.py
+++ app/endpoints/user/router.py
@@ -5,14 +5,13 @@ from app.endpoints.orders.dao import OrdersDAO
 
 router = APIRouter(prefix='/user', tags=['Пользователь'])
 
-@router.post("/update", summary="Обновление данных пользователя", response_model=STokens)
+@router.post("/update", summary="Обновление данных пользователя", response_model=SUserFull)
 async def update_user(body: SUserUpdate, authorization: str | None = Header(None)):
     user_service = UserService()
-    user = await user_service.update_user(body, authorization)
-
-    tokens = user_service.token_service.generate_tokens(user)
-
-    return tokens
+    await user_service.update_user(body, authorization)
+    
+    user = await user_service.get_current_user(authorization)
+    return user
 
 @router.get("/me", summary="Получение данных текущего пользователя", response_model=SUserFull)
 async def get_current_user(authorization: str | None = Header(None)):
